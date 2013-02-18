53195dc73c8588333346bdbdf7359f240a8956c8
shaubert <shaubert.alexander@gmail.com>
2013-02-18 15:49:17 +0400

    added ability to get mapper by converting classes

--- proto-mapper-processor/src/main/java/com/shaubert/protomapper/MappingAnnotationsProcessor.java
+++ proto-mapper-processor/src/main/java/com/shaubert/protomapper/MappingAnnotationsProcessor.java
@@ -2,12 +2,10 @@ package com.shaubert.protomapper;
 
 import com.shaubert.protomapper.annotations.Mapper;
 import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
@@ -16,6 +14,8 @@ import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.Writer;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
@@ -24,22 +24,70 @@ import java.util.Set;
 public class MappingAnnotationsProcessor extends AbstractProcessor {
 
     public static final String MAPPER_VM_FILE = "mapper.vm";
+    public static final String PROTO_MAPPERS_VM_FILE = "mapper-map.vm";
     private VelocityEngine velocityEngine;
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         if (!roundEnv.processingOver()) {
             processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, "searching for mappers");
+            List<ClassParams> paramsList = new ArrayList<ClassParams>();
             for (Element elem : roundEnv.getElementsAnnotatedWith(Mapper.class)) {
                 ClassParams mapperParams = new ClassParams((TypeElement) elem, roundEnv);
                 logMapper(mapperParams);
-                Template template = createTemplate();
+                paramsList.add(mapperParams);
+                Template template = createMapperTemplate();
                 if (template != null) {
                     createMapperClass(template, mapperParams);
                 }
             }
+            Template template = createProtoMappersTemplate();
+            if (template != null) {
+                createProtoMappersClass(template, paramsList);
+            }
         }
         return false;
+    }
+
+    private void createProtoMappersClass(Template template, List<ClassParams> mappers) {
+        JavaFileObject jfo = null;
+        Writer writer = null;
+        try {
+            jfo = processingEnv.getFiler().createSourceFile("com.shaubert.protomapper.ProtoMappers");
+
+            processingEnv.getMessager().printMessage(
+                    Diagnostic.Kind.NOTE,
+                    "creating source file: " + jfo.toUri());
+
+            writer = jfo.openWriter();
+
+            processingEnv.getMessager().printMessage(
+                    Diagnostic.Kind.NOTE,
+                    "applying velocity template: " + template.getName());
+
+            List<String> classNames = new ArrayList<String>(mappers.size());
+            for (ClassParams mapper : mappers) {
+                classNames.add(mapper.getClassName() + "Mapper");
+            }
+            VelocityContext context = new VelocityContext();
+            context.put("mappers", classNames);
+
+            template.merge(context, writer);
+
+            writer.close();
+        } catch (FilerException fillerEx) {//Attempt to recreate a file for type
+            //all mappers should be created in the first round of annotation processing
+            //so mapper is already created
+            //and we do nothing
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
     }
 
     private void createMapperClass(Template template, ClassParams mapperParams) {
@@ -80,7 +128,15 @@ public class MappingAnnotationsProcessor extends AbstractProcessor {
         processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
     }
 
-    private Template createTemplate() {
+    private Template createProtoMappersTemplate() {
+        return createTemplate(PROTO_MAPPERS_VM_FILE);
+    }
+
+    private Template createMapperTemplate() {
+        return createTemplate(MAPPER_VM_FILE);
+    }
+
+    private Template createTemplate(String fileName) {
         if (velocityEngine == null) {
             try {
                 velocityEngine = setupVelocityEngine();
@@ -90,7 +146,7 @@ public class MappingAnnotationsProcessor extends AbstractProcessor {
             }
         }
         try {
-            return velocityEngine.getTemplate(MAPPER_VM_FILE);
+            return velocityEngine.getTemplate(fileName);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
--- proto-mapper-sample/src/main/java/com/shaubert/protomapper/UsageOfMappersTest.java
+++ proto-mapper-sample/src/main/java/com/shaubert/protomapper/UsageOfMappersTest.java
@@ -1,9 +1,7 @@
 package com.shaubert.protomapper;
 
-import com.shaubert.protomapper.sample.protobuf.Services;
-
 public class UsageOfMappersTest {
     public void use() {
-        MapTestMapper.map((Services.Map)null);
+        new MapTestMapper();
     }
 }
